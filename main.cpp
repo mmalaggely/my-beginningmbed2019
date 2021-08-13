e341c99d27f93906f955e2ab66ee5a40a8104e3e
mmalaggely <mmalaggely@localhost>
2021-08-13 00:41:16 +0000

    servo motor example mbed

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Hello World to sweep a servo through its full range
+ 
+#include "mbed.h"
+#include "Servo.h"
+ 
+Servo myservo(p21);
+ 
+int main() {    
+    for(float p=0; p<1.0; p += 0.1) {
+        myservo = p;
+        wait(0.2);
+    }
+}
